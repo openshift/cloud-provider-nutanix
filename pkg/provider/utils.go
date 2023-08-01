e3c363fa8ded4e99400b93086cc4ad0cb73ee6b1
Yanhua Li <yanhua.li@nutanix.com>
2023-08-01 12:50:54 -0400

    UPSTREAM: 106: Nutanix CCM should scope secret informers per namespace

--- pkg/provider/utils.go
+++ pkg/provider/utils.go
@@ -8,7 +8,6 @@ import (
 	"github.com/nutanix-cloud-native/cloud-provider-nutanix/internal/constants"
 )
 
-// GetCCMNamespace returns the CCM controller pod namespace
 func GetCCMNamespace() (string, error) {
 	ns := os.Getenv(constants.CCMNamespaceKey)
 	if ns == "" {
@@ -17,7 +16,6 @@ func GetCCMNamespace() (string, error) {
 	return ns, nil
 }
 
-// NoResyncPeriodFunc returns the 0 resync period
 func NoResyncPeriodFunc() time.Duration {
 	return 0
 }
